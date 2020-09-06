76758ab2487b9943e1a515d925ef175d7e3ae372
noktaris <64233671+noktaris@users.noreply.github.com>
2020-09-06 16:05:10 -0400

    File restructure
    Update to split the files up.

---
+++ Tair_SpellHighlight.lua
@@ -0,0 +1,169 @@
+---------------------------------------------
+--
+--
+-- Tair_SpellHighlight.lua
+--
+--
+---------------------------------------------
+
+local name,addon=...;
+
+---------------------------------------------
+--
+-- Config
+--
+---------------------------------------------
+
+---------------------------------------------
+-- Create Frame
+---------------------------------------------
+
+addon.EventFrame = CreateFrame("Frame")
+addon.EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+addon.EventFrame:RegisterEvent("UNIT_AURA")
+addon.EventFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
+addon.EventFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
+addon.EventFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
+
+---------------------------------------------
+-- Get Action Bars
+---------------------------------------------
+
+-- Blizzard
+local ActionBars = { "Action", "MultiBarBottomLeft", "MultiBarBottomRight", "MultiBarRight", "MultiBarLeft" }
+
+-- ElvUI
+if IsAddOnLoaded("ElvUI") then
+	ActionBars = { "ElvUI_Bar1", "ElvUI_Bar2", "ElvUI_Bar3", "ElvUI_Bar4", "ElvUI_Bar5", "ElvUI_Bar6" }
+end
+
+---------------------------------------------
+--
+-- Functions
+--
+---------------------------------------------
+
+---------------------------------------------
+-- Start Spell Highlight
+---------------------------------------------
+
+function addon.Tair_SpellHighlight_Start(searchName)
+	-- Blizzard UI
+	if not IsAddOnLoaded("ElvUI") and not IsAddOnLoaded("AzeriteUI") then
+		for _, barName in pairs(ActionBars) do
+			for i = 1, 12 do
+				local buttonName = barName .. "Button" .. i
+				local button = _G[buttonName]
+				local slot = ActionButton_GetPagedID(button) or ActionButton_CalculateAction(button) or button:GetAttribute("action") or 0
+				if HasAction(slot) then
+					local actionType, id, _, actionName = GetActionInfo(slot)
+					if actionType == "macro" then
+						actionName, _, id = GetMacroSpell(id)
+					elseif actionType == "item" then
+						actionName = GetItemInfo(id)
+					elseif actionType == "spell" then
+						actionName = GetSpellInfo(id)
+					end
+					if actionName and string.match(string.lower(actionName), string.lower(searchName)) then
+						LibStub("LibButtonGlow-1.0").ShowOverlayGlow(button)
+						return button
+					end
+				end
+			end
+		end
+	end
+	-- End Blizzard UI
+	-- ElvUI
+	if IsAddOnLoaded("ElvUI") then
+		for _, barName in pairs(ActionBars) do
+			for i = 1, 12 do
+				local buttonName = barName .. "Button" .. i
+				local button = _G[buttonName]
+				local type = button:GetAttribute('type')
+				if type == 'action' then
+					local slot = button:GetAttribute('action')
+					local actionType, id, actionName = GetActionInfo(slot)
+					if actionType == "macro" then
+						actionName, _, id = GetMacroSpell(id)
+					elseif actionType == "item" then
+						actionName = GetItemInfo(id)
+					elseif actionType == "spell" then
+						actionName = GetSpellInfo(id)
+					end
+					if actionName and string.match(string.lower(actionName), string.lower(searchName)) then
+						LibStub("LibButtonGlow-1.0").ShowOverlayGlow(button)
+						return button
+					end
+				end
+			end
+		end
+	end
+	-- End ElvUI
+end
+
+---------------------------------------------
+-- End Spell Highlight
+---------------------------------------------
+
+function addon.Tair_SpellHighlight_End(searchName)
+	if not IsAddOnLoaded("ElvUI") and not IsAddOnLoaded("AzeriteUI") then
+		for _, barName in pairs(ActionBars) do
+			for i = 1, 12 do
+				local buttonName = barName .. "Button" .. i
+				local button = _G[buttonName]
+				local slot = ActionButton_GetPagedID(button) or ActionButton_CalculateAction(button) or button:GetAttribute("action") or 0
+				if HasAction(slot) then
+					local actionType, id, _, actionName = GetActionInfo(slot)
+					if actionType == "macro" then
+						actionName, _, id = GetMacroSpell(id)
+					elseif actionType == "item" then
+						actionName = GetItemInfo(id)
+					elseif actionType == "spell" then
+						actionName = GetSpellInfo(id)
+					end	
+					if actionName and string.match(string.lower(actionName), string.lower(searchName)) then
+						LibStub("LibButtonGlow-1.0").HideOverlayGlow(button)
+						return button
+					end
+				end
+			end
+		end
+	end 
+	if IsAddOnLoaded("ElvUI") then
+		for _, barName in pairs(ActionBars) do
+			for i = 1, 12 do
+				local buttonName = barName .. "Button" .. i
+				local button = _G[buttonName]
+				local type = button:GetAttribute('type')
+				if type == 'action' then
+					local slot = button:GetAttribute('action')
+					local actionType, id, actionName = GetActionInfo(slot)
+					if actionType == "macro" then
+						actionName, _, id = GetMacroSpell(id)
+					elseif actionType == "item" then
+						actionName = GetItemInfo(id)
+					elseif actionType == "spell" then
+						actionName = GetSpellInfo(id)
+					end
+					if actionName and string.match(string.lower(actionName), string.lower(searchName)) then
+						LibStub("LibButtonGlow-1.0").HideOverlayGlow(button)
+						return button
+					end
+				end
+			end
+		end
+	end
+end
+
+-- End Action Bar Functions
+
+---------------------------------------------
+--
+-- Chat Commmand Config
+--
+---------------------------------------------
+
+SLASH_TAIRPALADIN1 = '/tair';
+function SlashCmdList.TAIRPALADIN(msg, editbox)
+	print("Hello")
+end
---
+++ Tair_SpellHighlight_Paladin.lua
@@ -0,0 +1,212 @@
+---------------------------------------------
+--
+--
+-- Tair_SpellHighlight_Paladin.lua
+--
+--
+---------------------------------------------
+
+local name,addon=...;
+
+---------------------------------------------
+--
+-- Events
+--
+---------------------------------------------
+
+if UnitClass("player") == "Paladin" then
+	addon.EventFrame:HookScript("OnEvent", function(self, event, ...)
+		---------------------------------------------
+		-- PLAYER_ENTERING_WORLD
+		---------------------------------------------
+		if event == "PLAYER_ENTERING_WORLD" then
+			local ActiveHighlight_Seals = false;
+			local ActiveHighlight_Purify = false;
+			local ActiveHighlight_Judgement = false;
+		end
+		-- END PLAYER_ENTERING_WORLD
+		---------------------------------------------
+		-- PLAYER_REGEN_DISABLED
+		---------------------------------------------
+		-- Trigger a seal reminder if entering combat with no seal
+		if event == "PLAYER_REGEN_DISABLED" and not Tair_Paladin_SealActive() then
+			Tair_Paladin_HighlightSeals()
+		end
+		-- END PLAYER_REGEN_DISABLED
+		---------------------------------------------
+		-- PLAYER_REGEN_ENABLED
+		---------------------------------------------
+		-- Deactivate seal reminder when leaving combat
+		if event == "PLAYER_REGEN_ENABLED" and not Tair_Paladin_SealActive() then
+			Tair_Paladin_UnHighlightSeals();
+		end
+		-- END PLAYER_REGEN_ENABLED
+		---------------------------------------------
+		-- UNIT_AURA
+		---------------------------------------------
+		if event == "UNIT_AURA" then
+			local unit = ...
+			if unit == "player" then
+				-- If a seal is active
+				if Tair_Paladin_SealActive() then
+					-- Deactivate seal reminder
+					ActiveHighlight_Seals = false;
+					Tair_Paladin_UnHighlightSeals()
+				-- If player is in combat with no active seal
+				elseif not Tair_Paladin_SealActive() and UnitAffectingCombat("player") and ActiveHighlight_Seals == false then
+					-- Trigger a seal reminder
+					ActiveHighlight_Seals = true;
+					Tair_Paladin_HighlightSeals()
+					-- De-highlight Judgement when no seal is active
+					addon.Tair_SpellHighlight_End("Judgement")
+				end
+				-- Purify check
+				Tair_Paladin_CheckPurify()
+			end
+			if unit == "target" then
+				Tair_Paladin_CheckJudgement()
+				Tair_Paladin_BuffExchange()
+			end
+		end
+		-- END UNIT_AURA
+		---------------------------------------------
+		-- PLAYER_TARGET_CHANGED
+		---------------------------------------------
+		if event == "PLAYER_TARGET_CHANGED" then
+			Tair_Paladin_BuffExchange()
+		end -- end PLAYER_TARGET_CHANGED events
+		-- END PLAYER_TARGET_CHANGED
+
+	end) -- end SpellHighlight Events
+end -- end Paladin
+
+---------------------------------------------
+--
+-- Functions
+--
+---------------------------------------------
+
+---------------------------------------------
+-- Seal Check
+-- Returns true if the player has an active seal
+---------------------------------------------
+
+local Tair_Paladin_Seals = {
+	'Seal of Righteousness', 'Seal of the Crusader', 'Seal of Command', 'Seal of Light', 'Seal of Wisdom', 'Seal of Justice'
+}
+
+function Tair_Paladin_SealActive()
+	local sealActive = false;
+	for i, seal in ipairs(Tair_Paladin_Seals) do
+		if AuraUtil.FindAuraByName(seal, "player") then
+			sealActive = true
+		end
+	end
+	if sealActive == true then
+		return true
+	else
+		return false
+	end
+end
+
+---------------------------------------------
+-- Seal Highlight
+---------------------------------------------
+
+function Tair_Paladin_HighlightSeals()
+	Tair_Paladin_UnHighlightSeals()
+	local mainSpeed, offSpeed = UnitAttackSpeed("player")
+	if IsSpellKnown(20375) and (mainSpeed > 3.4) then
+		addon.Tair_SpellHighlight_Start("Seal of Command")
+	else
+		addon.Tair_SpellHighlight_Start("Seal of Righteousness")
+	end
+end
+
+---------------------------------------------
+-- Deactivate Seal Highlight
+---------------------------------------------
+
+function Tair_Paladin_UnHighlightSeals()
+	for i, seal in ipairs(Tair_Paladin_Seals) do
+		addon.Tair_SpellHighlight_End(seal)
+	end
+end
+
+---------------------------------------------
+-- Purify Check
+---------------------------------------------
+
+function Tair_Paladin_CheckPurify()
+	local NeedPurify = false;
+	for i = 1, 16 do
+	    local name, icon, count, debuffType = UnitDebuff("player", i);
+	    if debuffType == "Disease" or debuffType == "Poison" then
+			NeedPurify = true;
+		end
+		if NeedPurify ~= true then
+			NeedPurify = false;
+		end
+	end
+	if NeedPurify == true and ActiveHighlight_Purify == false then
+		ActiveHighlight_Purify = true;
+		addon.Tair_SpellHighlight_Start("Purify")
+	elseif NeedPurify == false then
+		ActiveHighlight_Purify = false;
+		addon.Tair_SpellHighlight_End("Purify")
+	end
+end
+
+---------------------------------------------
+-- Judgement Check
+---------------------------------------------
+
+function Tair_Paladin_TargetIsStunned()
+	if UnitExists("target") then
+		local stun = "Hammer of Justice";
+		for i = 1, 16 do
+			local name = UnitDebuff("target", i);
+			if name == stun then
+				return true
+			end
+		end
+	end
+end
+
+function Tair_Paladin_CheckJudgement()
+	if AuraUtil.FindAuraByName("Seal of Command", "player") and Tair_Paladin_TargetIsStunned() and IsUsableSpell("Judgement") and not ActiveHighlight_Judgement then
+		addon.Tair_SpellHighlight_Start("Judgement");
+		ActiveHighlight_Judgement = true;
+	else 
+		addon.Tair_SpellHighlight_End("Judgement");
+		ActiveHighlight_Judgement = false;
+	end
+end
+
+---------------------------------------------
+-- Blessing Exchange Highlight
+---------------------------------------------
+
+function Tair_Paladin_BuffExchange()
+	if UnitExists("target") and UnitClass("target") == "Paladin" and UnitIsFriend("player", "target") and not (UnitIsUnit("player","target")) then
+		-- Blessing of Might
+		if not AuraUtil.FindAuraByName("Blessing of Might", "target") then
+			addon.Tair_SpellHighlight_Start("Blessing of Might")
+		elseif not AuraUtil.FindAuraByName("Blessing of Wisdom", "target") then
+			addon.Tair_SpellHighlight_Start("Blessing of Wisdom")
+		else
+			Tair_Paladin_UnHighlightBlessings()
+		end
+	else
+		Tair_Paladin_UnHighlightBlessings()
+	end
+end
+
+---------------------------------------------
+-- Deactivate Blessing Highlight
+---------------------------------------------
+
+function Tair_Paladin_UnHighlightBlessings()
+	addon.Tair_SpellHighlight_End("Blessing of Might")
+	addon.Tair_SpellHighlight_End("Blessing of Wisdom")
+end
